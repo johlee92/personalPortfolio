92939b15b5c947bca932811eab93b283631c5086
JY <johnathanlee72@hotmail.com>
2020-04-15 23:15:05 -0600

    added a simple menu dropdown for the mobile page

--- projects.js
+++ projects.js
@@ -2,6 +2,15 @@
 
 const PROJECTS = [
     {
+        title: 'Relentless: Live with Intent',
+        screenshot: 'images/quizApp.png',
+        description: 'Algeba Quizz is, as its name implies, an app that tests the user\'s knowledge of algebra',
+        technologies: ['javascript','html','css'],
+        liveLink: 'https://johlee92.github.io/quizAppThinkful/',
+        repoLink: 'https://github.com/johlee92/quizAppThinkful',
+        clickCount: 0
+    },
+    {
         title: 'Algebra Quiz',
         screenshot: 'images/quizApp.png',
         description: 'Algeba Quizz is, as its name implies, an app that tests the user\'s knowledge of algebra',
@@ -10,23 +19,13 @@ const PROJECTS = [
         repoLink: 'https://github.com/johlee92/quizAppThinkful',
         clickCount: 0
     },
-    //need to fix code to make multiple projects work
-    // {
-    //     title: 'Algebra Quiz',
-    //     screenshot: 'images/quizApp.png',
-    //     description: 'Algeba Quizz is, as its name implies, an app that tests the user\'s knowledge of algebra',
-    //     technologies: ['javascript','html','css'],
-    //     liveLink: 'https://johlee92.github.io/quizAppThinkful/',
-    //     repoLink: 'https://github.com/johlee92/quizAppThinkful',
-    //     clickCount: 0
-    // },
-    // {
-    //     title: 'Algebra Quiz',
-    //     screenshot: 'images/quizApp.png',
-    //     description: 'Algeba Quizz is, as its name implies, an app that tests the user\'s knowledge of algebra',
-    //     technologies: ['javascript','html','css'],
-    //     liveLink: 'https://johlee92.github.io/quizAppThinkful/',
-    //     repoLink: 'https://github.com/johlee92/quizAppThinkful',
-    //     clickCount: 0
-    // }
+    {
+        title: 'Algebra Quiz',
+        screenshot: 'images/quizApp.png',
+        description: 'Algeba Quizz is, as its name implies, an app that tests the user\'s knowledge of algebra',
+        technologies: ['javascript','html','css'],
+        liveLink: 'https://johlee92.github.io/quizAppThinkful/',
+        repoLink: 'https://github.com/johlee92/quizAppThinkful',
+        clickCount: 0
+    }
 ];
